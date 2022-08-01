d9554d8b60e8c4036609542e94c589c3abeca8c8
cbrunie <clement.brunie@lyra-network.com>
2022-08-01 10:38:49 +0200

    Block zoom v4

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,21 @@ import GameBoy from "@/layout/GameBoy.vue";
 
 export default {
   components: { GameBoy },
+  created() {
+    // Disable pinch to zoom
+    document.addEventListener('touchmove', function (event) {
+      if (event.scale !== 1) { event.preventDefault(); }
+    }, false);
+
+    // Disable double tap zoom
+    let lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+      const now = (new Date()).getTime();
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+      }
+      lastTouchEnd = now;
+    }, false);
+  }
 };
 </script>
