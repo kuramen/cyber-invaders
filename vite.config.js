7a6a91c7bd409f17f9c3ef9b2712db38f8920a1e
cbrunie <clement.brunie@lyra-network.com>
2022-08-02 16:16:16 +0200

    optimize png vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteImagemin({
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
